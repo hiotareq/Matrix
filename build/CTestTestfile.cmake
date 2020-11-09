8b576df807ae32e535bda11c0af68cdf507e391c
hiotareq <62850531+hiotareq@users.noreply.github.com>
2020-11-09 12:21:07 +0300

    additional files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: c:/Users/bobro/Desktop/matrix/Matrix
+# Build directory: c:/Users/bobro/Desktop/matrix/Matrix/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
