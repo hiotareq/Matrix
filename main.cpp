78988d178ee2cb545e4f13cb56255b3b3e029c0a
hiotareq <62850531+hiotareq@users.noreply.github.com>
2020-11-09 23:01:59 +0300

    Added build files, gitignore files and updated method to (may be) work good enough

--- main.cpp
+++ main.cpp
@@ -1,44 +1,10 @@
 #include "matrix.hpp"
-#include <fstream>
-#include <cstdlib>
-#include <ctime>
 
-#if 0
-int main()
-{
-    matrix::matrix<int> matr;
-    std::fstream det;
-    det.open("C:\\Users\\bobro\\Desktop\\matrix\\Matrix\\identity_matrix_1000x1000.txt");
-    det >> matr;
-    std :: cout << matr.det();
-    return 0;
-}
-#endif
-
-int main(){
+int main() {
     int n;
     std::cin >> n;
-    matrix::matrix<double> matr(n);
+    matrix::matrix<long double> matr(n);
     std::cin >> matr;
     std::cout << matr.det();
     return 0;
 }
-
-#if 0
-int main()
-{
-    std::fstream det;
-    det.open("C:\\Users\\bobro\\Desktop\\matrix\\Matrix\\identity_matrix_1000x1000.txt");
-    int n = 1000;
-    det << n << std::endl;
-    matrix::matrix<int>* matr = new matrix::matrix<int>;
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < n; ++j){
-            (*matr)[i][j] = std::rand();
-        }
-    }
-    det << matr;
-    std::cout << (*matr).det() << std::endl;
-    return 0;
-}
-#endif
--- matrix.hpp
+++ matrix.hpp
@@ -8,182 +8,138 @@
 #include <numeric>
 #include <typeinfo>
 #include <cmath>
+#include <utility>
 
-namespace matrix
-{
-    template <typename T>
-    class matrix final
-    {
+namespace matrix {
+    template<typename T>
+    class matrix final {
     private:
         int n_; //количество строк/столбцов
         T **matrix_;
 
-        struct row
-        {
+        struct row {
             T *row_;
 
-            const T &operator[](int index) const
-            {
+            const T &operator[](int index) const {
                 return row_[index];
             }
 
-            T &operator[](int index)
-            {
+            T &operator[](int index) {
                 return row_[index];
             }
         };
 
     public:
-        int GetN() const
-        {
+        int GetN() const {
             return n_;
         }
 
-        matrix(int n)
-        {
+        matrix(int n) {
             n_ = n;
             matrix_ = new T *[n_];
-            for (int i = 0; i < n_; ++i)
-            {
+            for (int i = 0; i < n_; ++i) {
                 matrix_[i] = new T[n_];
+            }
+            for (int i = 0; i < n_; ++i) {
+                for (int j = 0; j < n_; ++j) {
+                    matrix_[i][j] = 0;
+                }
             }
         };
 
-        matrix() : matrix(0){};
+        matrix() : matrix(0) {};
 
-        ~matrix()
-        {
-            for (int i = 0; i < n_; ++i)
-            {
+        ~matrix() {
+            for (int i = 0; i < n_; ++i) {
                 delete[] matrix_[i];
             }
             delete[] matrix_;
         }
 
-        matrix(const matrix &old)
-        {
+        matrix(const matrix &old) {
             n_ = old.GetN();
             matrix_ = new T *[n_];
-            for (int i = 0; i < n_; ++i)
-            {
+            for (int i = 0; i < n_; ++i) {
                 matrix_[i] = new T[n_];
             }
 
-            for (int i = 0; i < n_; ++i)
-            {
-                for (int j = 0; j < n_; ++j)
-                {
+            for (int i = 0; i < n_; ++i) {
+                for (int j = 0; j < n_; ++j) {
                     matrix_[i][j] = old.matrix_[i][j];
                 }
             }
         }
 
-        matrix(int n, bool flag)
-        { //конструктор для единичной матрицы
+        matrix(int n, bool flag) { //конструктор для единичной матрицы
             n_ = n;
             matrix_ = new T *[n_];
-            for (int i = 0; i < n_; ++i)
-            {
+            for (int i = 0; i < n_; ++i) {
                 matrix_[i] = new T[n_];
             }
 
-            for (int i = 0; i < n_; ++i)
-            {
-                matrix_[i][i] = 1;
+            for (int i = 0; i < n_; ++i) {
+                for (int j = 0; j < n_; ++j) {
+                    if(j == i) matrix_[i][j] = 1;
+                    else    matrix_[i][j] = 0;
+                }
             }
         }
 
-        const row operator[](int index) const
-        {
+        row operator[](int index) {
             return row{matrix_[index]};
         }
 
-        row operator[](int index)
-        {
+        const row operator[](int index) const {
             return row{matrix_[index]};
         }
 
-        T det()
-        {
-            double mem = 1;
-            for (int i = 0; i < n_; ++i)
-            {
-                if (i == (n_ - 1))
-                {
-                    for(int k = 0; k < n_; ++k)
-                    {
-                        if(matrix_[k][i] != 0)
-                        {
-                            for(int j = 0; j < n_; ++j)
-                            {
-                                matrix_[i][j] += matrix_[k][j];
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                if (matrix_[i][i] == 0)
-                {
-                    for (int k = i + 1; k < n_; ++k)
-                    {
-                        if (matrix_[k][i] != 0)
-                        {
-                            T* swap = matrix_[i];
-                            matrix_[i] = matrix_[k];
-                            matrix_[k] = swap;
-                            mem *= pow((-1), (k - i));
+        T det() {
+            long double mem = 1;//variable to remember actions with determinant
+            for (int i = 0; i < n_; ++i) {
+                //////////////////////////////////////////////////////////////////////////////
+                //checking if matrix_[i][i] == 0, because we mustn't have it in Gauss method//
+                //////////////////////////////////////////////////////////////////////////////
+                if (matrix_[i][i] == 0) {
+                    for (int j = i + 1; j < n_; ++j) {
+                        if (matrix_[j][i] != 0) {
+                            std::swap(matrix_[i], matrix_[j]);
+                            mem *= std::pow(-1, j - i);
                             break;
                         }
-                        if (k == n_ - 1)
-                            return 0; //here is col with 0-s
-                    }                 
-                }//after these actions matrix_[i][i] != 0
-
-                for (int k = i + 1; k < n_; ++k)
-                {
-                    if (matrix_[k][i] == 0)
-                    {
-                        continue;
                     }
+                }
+                //////////////////////////////////////////////////////////////
+                //after these actions we don't have cells on diagonal with 0//
+                //////////////////////////////////////////////////////////////
 
+                for (int k = i + 1; k < n_; ++k) {//take a look at cells under diagonal
 
-                    mem /= matrix_[i][i];
-                    mem *= matrix_[k][i];
-                    double tmp1 = matrix_[i][i], tmp2 = matrix_[k][i];
-                    std::cout << "tmp1[" << i << "] = " << tmp1 << std::endl;
-
-                    for (int c = 0; c < n_; ++c)
-                    {
-                        matrix_[i][c] /= tmp1;
-                        matrix_[i][c] *= tmp2;
-                    }
-
-                    for (int c = 0; c < n_; ++c)
-                        matrix_[k][c] -= matrix_[i][c];
-
-                    for(int c = 0; c < n_; ++c)
-                        matrix_[i][c] *= tmp1;
+                    if (matrix_[k][i] != 0) {
+                        std::cout << matrix_[i][i]<< std::endl;
+                        long double tmp = matrix_[k][i] / matrix_[i][i];
+                        mem /= tmp;
+                        for (int j = 0; j < n_; ++j) {
+                            matrix_[i][j] *= tmp;
+                            matrix_[k][j] -= matrix_[i][j];//make cells under diagonal equal zero
+                        }
+                    }//else matrix_[k][i] == 0, do nothing
                 }
             }
-
-            double det = 1;
-            for (int i = 0; i < n_; ++i)
+            long double det = 1;
+            for (int i = 0; i < n_; ++i) {
                 det *= matrix_[i][i];
+            }
 
-            det /= mem;
+            det *= mem;
             return det;
         }
     };
 } // namespace matrix
 
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const matrix::matrix<T> &matr)
-{
-    for (int i = 0; i < matr.GetN(); ++i)
-    {
-        for (int j = 0; j < matr.GetN(); ++j)
-        {
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const matrix::matrix<T> &matr) {
+    for (int i = 0; i < matr.GetN(); ++i) {
+        for (int j = 0; j < matr.GetN(); ++j) {
             os << matr[i][j] << " ";
         }
         os << std::endl;
@@ -191,13 +147,10 @@ std::ostream &operator<<(std::ostream &os, const matrix::matrix<T> &matr)
     return os;
 }
 
-template <typename T>
-std::istream &operator>>(std::istream &is, matrix::matrix<T> &matr)
-{
-    for (int i = 0; i < matr.GetN(); ++i)
-    {
-        for (int j = 0; j < matr.GetN(); ++j)
-        {
+template<typename T>
+std::istream &operator>>(std::istream &is, matrix::matrix<T> &matr) {
+    for (int i = 0; i < matr.GetN(); ++i) {
+        for (int j = 0; j < matr.GetN(); ++j) {
             is >> matr[i][j];
         }
     }
